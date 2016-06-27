5fbd7caa2df8263951441361fb1cb97bc5f5836a
Wenjie Guo <wguo@redhat.com>
2016-06-27 17:04:54 +0800

    Extract js ref to a common file

---
+++ src/main/webapp/common.js
@@ -0,0 +1,25 @@
+document.write('<link rel="stylesheet" href="../patternfly/dist/css/patternfly.min.css"\/>');
+// <!-- jQuery -->
+document.write('<script src="../3rdlib/jquery/dist/jquery.min.js"><\/script>');
+
+// <!-- Bootstrap JS -->
+document.write('<script src="../3rdlib/bootstrap/dist/js/bootstrap.min.js"><\/script>');
+
+// <!-- Datatables, jQuery Grid Component -->
+// <!-- Note: jquery.dataTables.js must occur in the html source before patternfly*.js.-->
+document.write('<script src="../3rdlib/datatables/media/js/jquery.dataTables.min.js"><\/script>');
+
+// <!-- PatternFly Custom Componets -  Sidebar, Popovers and Datatables Customizations -->
+// <!-- Note: jquery.dataTables.js must occur in the html source before patternfly*.js.-->
+document.write('<script src="../patternfly/dist/js/patternfly.min.js"><\/script>');
+
+// <!-- Bootstrap Combobox -->
+document.write('<script src="../3rdlib/patternfly-bootstrap-combobox/js/bootstrap-combobox.js"><\/script>');
+// <!-- Bootstrap Select -->
+document.write('<script src="../3rdlib/bootstrap-select/dist/js/bootstrap-select.min.js"><\/script>');
+// <!-- Bootstrap Switch -->
+document.write('<script src="../3rdlib/bootstrap-switch/dist/js/bootstrap-switch.min.js"><\/script>');
+// <!-- Google Code Prettify - Syntax highlighting of code snippets -->
+document.write('<script src="../3rdlib/google-code-prettify/bin/prettify.min.js"><\/script>');
+// <!-- Moment - required by Bootstrap Date Time Picker -->
+document.write('<script src="../3rdlib/moment/min/moment.min.js"><\/script>');
